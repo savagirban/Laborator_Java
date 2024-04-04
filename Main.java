c2c25e4a4beba424061290afe07d7cecfb42a0de
Sava <66175303+savagirban@users.noreply.github.com>
2024-04-04 09:36:45 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,7 @@
-import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
-import java.util.Scanner;
+package org.example;
 
 public class Main {
     public static void main(String[] args) {
-      Judete judete = new Judete();
-        try {
-            judete.loadFromFile("./src/judete_in.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        Scanner scanner= new Scanner(System.in);
-        System.out.print("Judetul:");
-        scanner.nextLine();
-
+        System.out.println("Hello world!");
     }
-
 }
